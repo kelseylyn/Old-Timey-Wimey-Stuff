b4ff18cd56bd87c0a17f2d13db99a52374addb49
unknown <chc238u19@CHC238W19.Instruction.network.bcc>
2014-05-15 18:56:17 -0700

    added display, capture, edit to html. also added new functions to js

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,67 @@
 // This is the main javascript source for old Timey Wimey Stuff
+var photos = [];
+
+loadAllPhotos();
+hideAllViews();
+
+displayAllPhotos();
+$('#showall').show();
+
+function displayAllPhotos() {
+	//Start here on Tuesday, May 20
+}
+
+function loadAllPhotos() {
+	if(localStorage["photos"] != null) {
+		photos = JSON.parse(localStorage["photos"]);
+	}
+}
+
+function makePhotoEntry() {
+	var imageData = $('#camera-photo').attr('src').replace("data:image/jpeg;base64,", "");
+	var longitude = $('#longitude').html();
+	var latitude = $('#latitude').html();
+	var description = $('#description').val();
+
+	var photoEntry = {
+		"image" : imageData,
+		"longitude" : longitude,
+		"latitude" : latitude,
+		"description" : description
+	};
+
+	photose.push(photoEntry);
+}
+
+function saveAllPhotos() {
+	localStorage.clear();
+	localStorage["photos"] = JSON.stringigy(photos);
+	if(navigator.notification) {
+		navigator.notification.alert("Photo has been saved", null, "Success!", "OK");
+	}
+}
+
+$('button.save').click(function() {
+	makePhotoEntry();
+	saveAllPhotos();
+});
+
+function hideAllViews() {
+	$('#showall').hide();
+	$('#camera').hide();
+	$('#edit').hide();
+}
+
+$('li.viewlink').click(function() {
+	hideAllViews();
+	if($(this).html() == "Home") {
+		$('#showall').show();
+	} else if($(this).html() == "Capture") {
+		$('#camera').show();
+	} else {
+		$('#edit').show();
+	}
+});
 
 $('button.camera-control').click(function(){
 	//navigator is PhoneGap access to hardware
@@ -10,10 +73,9 @@ $('button.camera-control').click(function(){
 		encodingType: 0
 		};
 	
-		navigator.camera.getPicture(getPhoto, null, options);
-		navigator.geolocation.getCurrentPosition(getPosition,null,{enableHighAccuracy: true});
+	navigator.camera.getPicture(getPhoto, null, options);
+	navigator.geolocation.getCurrentPosition(getPosition,null,{enableHighAccuracy: true});
 	}
-
 });
 
 function getPhoto(data){
