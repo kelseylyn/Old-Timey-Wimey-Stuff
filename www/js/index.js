3db1b4819fcec6d276695787fdee48002a64f631
unknown <chc238u19@CHC238W19.Instruction.network.bcc>
2014-05-22 19:21:57 -0700

    changed html and js

--- www/js/index.js
+++ www/js/index.js
@@ -4,14 +4,9 @@ var photos = [];
 loadAllPhotos();
 hideAllViews();
 
-displayAllPhotos();
+populateEditList();
+populateList();
 $('#showall').show();
-
-function displayAllPhotos() {
-	for(var 1 = 0; i < photos.length; i++) {
-		$("#showall ul").append("<li>" + phtos[i]["imagepath"] + "</li>");
-	}
-}
 
 function loadAllPhotos() {
 	if(localStorage["photos"] != null) {
@@ -23,13 +18,13 @@ function makePhotoEntry() {
 	// var imageData = $('#camera-photo').attr('src').replace("data:image/jpeg;base64,", "");
 	var imageData = $('#camera-photo').attr('src');
 	var imagePath = $('#image-path').html();
-	var longitude = $('#longitude').html().replace('Long: ', ''); //MAY 20
-	var latitude = $('#latitude').html().replace('Lat: ', ''); //MAY 20
+	var longitude = $('#longitude').html();
+	var latitude = $('#latitude').html();
 	var description = $('#description').val();
 
 	var photoEntry = {
-		"image" : imageData,
-		"imagepath": imagepath,
+		"imageData" : imageData,
+		"imagePath": imagepath,
 		"longitude" : longitude,
 		"latitude" : latitude,
 		"description" : description
@@ -43,14 +38,27 @@ function saveAllPhotos() {
 	localStorage["photos"] = JSON.stringigy(photos);
 	if(navigator.notification) {
 		navigator.notification.alert("Photo has been saved", null, "Success!", "OK");
+	} else {
+		alert("Photo has been saved");
+	}
+}
+
+function saveAllPhotosAfterEdit () {
+	localStorage.clear();
+	localStorage["photos"] = JSON.stringify(photos);
+	if (navigator.notification) {
+		navigator.notification.alert("Changes have been saved", null, "Success!", "OK");
+	} else {
+		alert("Changes have been saved");
 	}
 }
 
 $('button.save').click(function() {
+	clearNewView();
 	makePhotoEntry();
 	saveAllPhotos();
-	$('#showall ul').children().remove();
-	displayAllPhotos;
+	populateList();
+	populateEditList();
 });
 
 function hideAllViews() {
@@ -59,63 +67,130 @@ function hideAllViews() {
 	$('#edit').hide();
 }
 
-$('li.viewlink').click(function() {
+$('li.viewlink').click(function () {
 	hideAllViews();
-	if($(this).html() == "Home") {
+	if ($(this).html() == "Home") {
+		clearFullView();
 		$('#showall').show();
-	} else if($(this).html() == "Capture") {
+	} else if ($(this).html() == "Capture") {
 		$('#camera').show();
 	} else {
-		populateEditView();
+		clearEditView();
 		$('#edit').show();
 	}
 });
 
-function populateEditView() {
-	var photoToEdit = photos[WHICH_PHOTO_???];
-
-	$('#camera-photo-edit').attr('src', photoToEdit["imageData"]);
-	$('#image-path-edit').html(photoToEdit["imagepath"]);
-	$('#longitude-edit').html(photoToEdit["longitude"]);
-	$('#latitude-edit').html(photoToEdit["latitude"]);
-	$('#description-edit').html(photoToEdit["description"]);
-}
-
-$("#save-edit").click(function() {
-	var imageData = $('#camera-photo').attr('src');
-	var imagePath = $('#image-path').html();
-	var longitude = $('#longitude').html() //.replace('Long: ', '');
-	var latitude = $('#latitude').html() // .replace('Lat: ', '');
-	var description = $('#description').val();
-
-	photoToEdit = photos[WHICH_PHOTO_???]["imageData"] = imageData;
-	photoToEdit = photos[WHICH_PHOTO_???]["imagepath"] = imagepath;
-	photoToEdit = photos[WHICH_PHOTO_???]["longitude"] = longitude;
-	photoToEdit = photos[WHICH_PHOTO_???]["latitude"] = latitude;
-	photoToEdit = photos[WHICH_PHOTO_???]["description"] = description;
-
-	saveAllPhotos();
-})
-
-$('button.camera-control').click(function(){
-	//navigator is PhoneGap access to hardware
-	if(navigator.camera) {
-		var options = {
-		quality: 60,
-		destinationType: Camera.DestinationType.FILE_URI,
-		sourceType: 1,
-		encodingType: 0
-		};
-	
-	navigator.camera.getPicture(getPhoto, null, options);
-	navigator.geolocation.getCurrentPosition(getPosition,null,{enableHighAccuracy: true});
-	}
+$('#edit ul').on('click', 'li', function () {
+	populateEditView($(this).attr('index'));
 });
 
-function getPhoto(imageURI){
-	$('#camera-photo').attr('src', imageURI);
-	window.resolveLocalFileSystemURI(imageURI, resolveOnSuccess, resolveOnError);
+function populateEditList () {
+	$('#edit ul').children().remove();
+	for (var i=0; i<photos.length; i++) {
+		var newRow = '<li index="' + i + '">';
+		newRow += '<img class="thumbnail" src="file:///mnt/sdcard' + photos[i]["imagePath"] + '" />';
+		newRow += '</li>';
+		$('#edit ul').append(newRow);
+	}
 }
+
+function populateEditView (index) {
+	var photoToEdit = photos[index];
+
+	$("#camera-photo-edit").attr('src', 'file:///mnt/sdcard' + photoToEdit["imagePath"]);
+	$("#image-path-edit").html(photoToEdit["imagePath"]);
+	$("#longitude-edit").val(photoToEdit["longitude"]);
+	$("#latitude-edit").val(photoToEdit["latitude"]);
+	$("#description-edit").val(photoToEdit["description"]);
+	$("#index-edit").html(index);
+}
+
+function clearEditView () {
+	$("#camera-photo-edit").attr('src', '');
+	$("#image-path-edit").html('');
+	$("#longitude-edit").val('');
+	$("#latitude-edit").val('');
+	$("#description-edit").val('');
+	$("#index-edit").html('');	
+}
+
+function clearFullView () {
+	$("#camera-photo-view").attr('src', '');
+	$("#image-path-view").html('');
+	$("#longitude-view").html('');
+	$("#latitude-view").html('');
+	$("#description-view").html('');
+	$("#index-view").html('');	
+}
+
+function clearNewView () {
+	$("#camera-photo").attr('src', '');
+	$("#image-path").html('');
+	$("#longitude").html('');
+	$("#latitude").html('');
+	$("#description").val('');
+	$("#index").html('');	
+}
+
+$('#showall ul').on('click', 'li', function () {
+	populateView($(this).attr('index'));
+});
+
+function populateList () {
+	$('#showall ul').children().remove();
+	for (var i=0; i<photos.length; i++) {
+		var newRow = '<li index="' + i + '">';
+		newRow += '<img class="thumbnail" src="file:///mnt/sdcard' + photos[i]["imagePath"] + '" />';
+		newRow += '</li>';
+		$('#showall ul').append(newRow);
+	}
+}
+
+function populateView (index) {
+	var photoToView = photos[index];
+
+	$("#camera-photo-view").attr('src', 'file:///mnt/sdcard' + photoToView["imagePath"]);
+	$("#image-path-view").html(photoToView["imagePath"]);
+	$("#longitude-view").html(photoToView["longitude"]);
+	$("#latitude-view").html(photoToView["latitude"]);
+	$("#description-view").html(photoToView["description"]);
+	$("#index-view").html(index);
+}
+
+$("button.save-edit").click(function () {
+	var imageData = $('#camera-photo-edit').attr('src');
+	var imagePath = $('#image-path-edit').html();
+	var longitude = $('#longitude-edit').val();
+	var latitude = $('#latitude-edit').val();
+	var description = $('#description-edit').val();
+	var index = $('#index-edit').html();
+
+	photos[index]["imageData"] = imageData;
+	photos[index]["imagePath"] = imagePath;
+	photos[index]["longitude"] = longitude;
+	photos[index]["latitude"] = latitude;
+	photos[index]["description"] = description;
+
+	saveAllPhotosAfterEdit();
+});
+
+$('button.camera-control').click(function () {
+    // navigator is PhoneGap access to hardware
+    if (navigator.camera) {
+        
+        var options = {
+            quality: 60,
+            //destinationType: Camera.DestinationType.DATA_URL,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: 1,
+            encodingType: 0
+        };
+        
+        navigator.camera.getPicture(getPhoto, null, options);
+        navigator.geolocation.getCurrentPosition(getPosition, null, {enableHighAccuracy: true});
+    }
+});
+
 function getPosition (position){
 	var longitude = position.coords.longitude;
 	var latitude = position.coords.latitude;
@@ -123,8 +198,10 @@ function getPosition (position){
 	$('#longitude').html('Long:' + longitude);
 	$('#latitude').html('Lat:'+ latitude);
 }
-function positionError (error) {
-	$('#error-output').html(error.message);
+
+function getPhoto(imageURI){
+	$('#camera-photo').attr('src', imageURI);
+	window.resolveLocalFileSystemURI(imageURI, resolveOnSuccess, resolveOnError);
 }
 
 function resolveOnError(error) {
